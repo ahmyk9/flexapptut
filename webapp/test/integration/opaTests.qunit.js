c6c85d77268a531b0a4c8325494c8fa12d5b8b9b
Ahmed Khilji <ahmed.khilji@epiuse.com.au>
2025-03-23 02:36:13 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["flexapptut/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"flexapptut/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
