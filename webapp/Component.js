dc9abfca7c8da3e977dd5fa3f71992bd3ca6ef37
Ahmed Khilji <ahmedkhilji99@gmail.com>
2025-03-28 15:54:50 +1100

    Step 7

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,24 +1,47 @@
 sap.ui.define([
 	'sap/ui/core/UIComponent',
-	'sap/ui/model/json/JSONModel'
-], function(UIComponent, JSONModel) {
+	'sap/ui/model/json/JSONModel',
+	'sap/f/library'
+], function(UIComponent, JSONModel, fioriLibrary) {
 	'use strict';
 
 	return UIComponent.extend("flexapptut.controller.Component", {
+
 
 		metadata: {
 			manifest: 'json'
 		},
 
 		init: function () {
-			var oProductsModel;
+			var oModel,
+				oProductsModel,
+				oRouter;
 
 			UIComponent.prototype.init.apply(this, arguments);
+
+			oModel = new JSONModel();
+			this.setModel(oModel);
 
 			// set products demo model on this sample
 			oProductsModel = new JSONModel('/model/products.json');
 			oProductsModel.setSizeLimit(1000);
 			this.setModel(oProductsModel, 'products');
+
+			oRouter = this.getRouter();
+			oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+			oRouter.initialize();
+		},
+
+		_onBeforeRouteMatched: function(oEvent) {
+			var oModel = this.getModel(),
+				sLayout = oEvent.getParameters().arguments.layout;
+
+			// If there is no layout parameter, set a default layout (normally OneColumn)
+			if (!sLayout) {
+				sLayout = fioriLibrary.LayoutType.OneColumn;
+			}
+
+			oModel.setProperty("/layout", sLayout);
 		}
 	});
 });
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -14,34 +14,37 @@ sap.ui.define([
 				this.oView = this.getView();
 				this._bDescendingSort = false;
 				this.oProductsTable = this.oView.byId("productsTable");
+				this.oRouter = this.getOwnerComponent().getRouter();
 			},
-	
+
 			onSearch: function (oEvent) {
 				var oTableSearchState = [],
 					sQuery = oEvent.getParameter("query");
-	
+
 				if (sQuery && sQuery.length > 0) {
 					oTableSearchState = [new Filter("Name", FilterOperator.Contains, sQuery)];
 				}
-	
+
 				this.oProductsTable.getBinding("items").filter(oTableSearchState, "Application");
 			},
-	
+
 			onAdd: function () {
 				MessageBox.information("This functionality is not ready yet.", {title: "Aw, Snap!"});
 			},
-	
+
 			onSort: function () {
 				this._bDescendingSort = !this._bDescendingSort;
 				var oBinding = this.oProductsTable.getBinding("items"),
 					oSorter = new Sorter("Name", this._bDescendingSort);
-	
+
 				oBinding.sort(oSorter);
 			},
-			onListItemPress: function () {
-				var oFCL = this.oView.getParent().getParent();
-	
-				oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
+
+			onListItemPress: function (oEvent) {
+				var productPath = oEvent.getSource().getBindingContext("products").getPath(),
+					product = productPath.split("/").slice(-1).pop();
+
+				this.oRouter.navTo("detail", {layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded, product: product});
 			}
 		});
 });
